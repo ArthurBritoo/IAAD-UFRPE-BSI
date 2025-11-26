3a1aec3760f4d19f2028973130b49d796e0d1428
Isadora <isadoraalbuqueques@gmail.com>
2025-11-26 13:59:09 -0300

    add trigger

---
+++ trigger.sql
@@ -0,0 +1,16 @@
+-- impedir a marcacao de consultas fora do horario
+DROP TRIGGER IF EXISTS trg_horario_comercial;
+DELIMITER $$
+
+CREATE TRIGGER trg_horario_comercial
+BEFORE INSERT ON consulta
+FOR EACH ROW
+BEGIN
+    -- bloqueia fora do horário comercial 08:00:00 - 18:00:00
+    IF TIME(NEW.Data_Hora) < '08:00:00' OR TIME(NEW.Data_Hora) > '18:00:00' THEN        
+        SIGNAL SQLSTATE '45000'
+		SET MESSAGE_TEXT = 'Consultas só podem ser marcadas entre 08:00 e 18:00.';
+    END IF;
+END$$
+DELIMITER ;
+
